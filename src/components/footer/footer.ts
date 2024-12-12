2abef6c890eb061da72f86f8051af654a59046c7
deeppines <atheus92@gmail.com>
2024-12-12 22:53:11 +0300

    refactor: footer

--- src/components/footer/footer.ts
+++ src/components/footer/footer.ts
@@ -1,12 +1,17 @@
 import style from './footer.module.scss';
 
 const footer = () => {
-  const since = '2020';
+  const since = '2017';
   const current = new Date().getFullYear();
 
   return `
     <footer class=${style.root}>
-      <p>©${since}-${current}</p>
+      <div>
+        <p>Created by <a href="https://github.com/deeppines">deeppines</a></p>
+      </div>
+      <div>
+        <p>©${since}-${current}</p>
+      </div>
     </footer>
   `;
 };
