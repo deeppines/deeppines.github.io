497c5bb7fcbe108b60828a453162be21f2e09f17
deeppines <atheus92@gmail.com>
2024-12-14 11:04:05 +0300

    refactor: footer layout

--- src/components/footer/footer.ts
+++ src/components/footer/footer.ts
@@ -1,16 +1,15 @@
+import copyright from '../copyright/copyright';
+import socials from '../socials/socials';
 import style from './footer.module.scss';
 
 const footer = () => {
-  const since = '2017';
-  const current = new Date().getFullYear();
-
   return `
     <footer class=${style.root}>
       <div>
-        <p>Created by <a href="https://github.com/deeppines">deeppines</a></p>
+        ${socials()}
       </div>
       <div>
-        <p>©${since}-${current}</p>
+        ${copyright()}
       </div>
     </footer>
   `;
