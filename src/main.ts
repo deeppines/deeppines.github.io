bf3a22d918df191de1bb45b07945bc630e0a88ca
deeppines <atheus92@gmail.com>
2024-12-14 10:56:04 +0300

    refactor: move styles to assets

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import footer from './components/footer/footer';
 import header from './components/header/header';
 
-import './styles/style.scss';
+import './assets/styles/style.scss';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   ${header()}
