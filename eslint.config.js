98c8396791a1e3a8d28d178d4e8369468a97e39d
deeppines <atheus92@gmail.com>
2024-12-17 18:43:48 +0300

    build: add import sort plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,62 @@
-import globals from 'globals';
 import pluginJs from '@eslint/js';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default [
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     files: ['**/*.{js,mjs,cjs,ts}'],
-  },
-  {
     languageOptions: {
       globals: globals.browser,
     },
   },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': [
+        'warn',
+        {
+          groups: [
+            // Common types
+            ['^@?\\/types'],
+            // Common constants
+            ['^@constants?\\/'],
+            // Utils
+            ['^@?\\/utils'],
+            // Api
+            ['^@?\\/api'],
+            // Services
+            ['^@?\\/services'],
+            // UI Pages
+            ['^@?\\/ui\\/pages'],
+            // UI Layouts
+            ['^@?\\/ui\\/layouts'],
+            // UI Elements
+            ['^@?\\/ui\\/elements'],
+            // UI Components
+            ['^@?\\/ui\\/components'],
+            // UI Other
+            ['^@?\\/ui\\/[^ce]', '^@?\\/ui\\/e', '^@?\\/ui\\/c'],
+            // Side effect imports.
+            ['^\\u0000'],
+            // Parent imports. Put `..` last.
+            ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+            // Utils relative imports.
+            ['^\\./utils'],
+            // Components relative imports.
+            ['^\\./components'],
+            // Other relative imports. Put same-folder imports and `.` last.
+            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+            // Style imports.
+            ['^.+\\.s?css$'],
+          ],
+        },
+      ],
+      'simple-import-sort/exports': 'error',
+    },
+  },
 ];
