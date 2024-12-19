2aa6fb59d4a2347397b6e55ee0b27f3a8ea8bae0
deeppines <atheus92@gmail.com>
2024-12-19 19:06:20 +0300

    refactor: added folders alias

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,29 @@
+import { fileURLToPath, URL } from 'url';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
     port: 3000,
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url)),
+      },
+      {
+        find: '@assets',
+        replacement: fileURLToPath(new URL('./src/assets', import.meta.url)),
+      },
+      {
+        find: '@components',
+        replacement: fileURLToPath(new URL('./src/ui/components', import.meta.url)),
+      },
+      {
+        find: '@layouts',
+        replacement: fileURLToPath(new URL('./src/ui/layouts', import.meta.url)),
+      },
+    ],
   },
   css: {
     preprocessorOptions: {
