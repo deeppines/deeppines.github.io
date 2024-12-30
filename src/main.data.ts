25aa5d14d6423edd97b6eb976562bc32beb10398
deeppines <atheus92@gmail.com>
2024-12-30 13:38:50 +0300

    feat: about data

--- src/main.data.ts
+++ src/main.data.ts
@@ -3,7 +3,8 @@ export const MAIN = {
     about: {
       id: 'modal-about',
       title: 'О странице',
-      content: 'Личный хомяк для различных экспериментов.',
+      content:
+        '<p>Личный хомяк создал эту страницу.</p><p>А предыдущую строку написала нейронка.</p><p>Что использовал:</p><ul><li>Vite</li><li>Typescript</li><li>SCSS modules</li></ul>',
     },
   },
 };
