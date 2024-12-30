73f8161ef90bb05bdcfe0b232d42ae9b0bcddd29
deeppines <atheus92@gmail.com>
2024-12-30 12:56:45 +0300

    feat: modal data

---
+++ src/main.data.ts
@@ -0,0 +1,9 @@
+export const MAIN = {
+  modals: {
+    about: {
+      id: 'modal-about',
+      title: 'О странице',
+      content: 'Личный хомяк для различных экспериментов.',
+    },
+  },
+};
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import footer from '@layouts/footer/footer';
 import header from '@layouts/header/header';
 
+import backdrop from '@components/backdrop/backdrop';
+import modal from '@components/modal/modal';
 import profile from '@components/profile/profile';
+import { PROFILE } from '@components/profile/profile.data';
 
-import { PROFILE } from './ui/components/profile/profile.data';
+import { MAIN } from './main.data';
 
 import '@assets/styles/style.scss';
 
@@ -15,4 +18,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     </section>
   </main>
   ${footer()}
+
+  ${modal(MAIN.modals.about)}
+  ${backdrop()}
 `;
