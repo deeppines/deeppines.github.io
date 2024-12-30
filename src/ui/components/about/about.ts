8324cf28568f3a59c1f3f8e4bca56ab81ee1a72b
deeppines <atheus92@gmail.com>
2024-12-30 12:49:40 +0300

    refactor: trigger

--- src/ui/components/about/about.ts
+++ src/ui/components/about/about.ts
@@ -4,7 +4,7 @@ import styles from './about.module.scss';
 
 const about = () => {
   return `
-    <button type="button" class="${styles.root}" title="About" onclick="showAbout()">
+    <button type="button" class="${styles.root} js-modal-open" title="About">
       <div class="${styles.icon}">${iconHelp}</div>
     </button>
   `;
