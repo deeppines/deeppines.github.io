7fb1dca2d19308f7f5cf0a430cdc39186292434a
deeppines <atheus92@gmail.com>
2025-01-08 15:04:15 +0300

    refactor: change about data

--- src/main.data.ts
+++ src/main.data.ts
@@ -4,7 +4,7 @@ export const MAIN = {
       id: 'modal-about',
       title: 'О странице',
       content:
-        '<p>Личный хомяк создал эту страницу.</p><p>А предыдущую строку написала нейронка.</p><p>Что использовал:</p><ul><li>Vite</li><li>Typescript</li><li>SCSS modules</li></ul>',
+        '<p>Что использовал:</p><ul><li>Vite</li><li>Typescript</li><li>CSS modules</li></ul>',
     },
   },
 };
