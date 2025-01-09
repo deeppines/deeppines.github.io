ab6dfde60232e796ec8229486920540a1af2de8e
deeppines <atheus92@gmail.com>
2025-01-09 08:54:42 +0300

    refactor: projects style

--- src/ui/components/projects/projects.ts
+++ src/ui/components/projects/projects.ts
@@ -6,6 +6,7 @@ import styles from './projects.module.scss';
 
 const projects = () => {
   return `
+    <h2 class="${styles.title}">Проекты</h2>
     <div class="${styles.root}">
       ${PROJECTS.map((item) => project(item)).join('')}
     </div>
