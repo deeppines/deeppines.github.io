6341da0c7009d86670f68c4afeec10f5bed8652c
deeppines <atheus92@gmail.com>
2025-01-09 08:54:01 +0300

    refactor: projects data

--- src/ui/components/projects/projects.data.ts
+++ src/ui/components/projects/projects.data.ts
@@ -3,7 +3,9 @@ import { ProjectProps } from './components/project/project';
 export const PROJECTS: ProjectProps[] = [
   {
     title: 'Pines',
-    description: 'Темная тема для Visual Studio Code',
+    description: 'Темная тема для VS Code',
     image: '/assets/img/projects/pines.png',
+    badge: 'https://vsmarketplacebadges.dev/version-short/deeppines.pines-visual-studio-code.svg',
+    link: 'https://marketplace.visualstudio.com/items?itemName=deeppines.pines-visual-studio-code',
   },
 ];
