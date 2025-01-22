caf9bcb169b416813da57f1a6981a6416b0589b4
deeppines <atheus92@gmail.com>
2025-01-22 19:28:58 +0300

    feat: lang select for header

--- src/ui/layouts/header/header.ts
+++ src/ui/layouts/header/header.ts
@@ -1,12 +1,16 @@
 import style from './header.module.scss';
 
+import lang from '@/ui/components/lang/lang';
 import switcher from '@/ui/components/switcher/switcher';
 
 const header = () => {
   return `
     <header class=${style.root}>
       <h1></h1>
-      <div>${switcher()}</div>
+      <div class="${style.buttons}">
+        ${switcher()}
+        ${lang()}
+      </div>
     </header>
   `;
 };
