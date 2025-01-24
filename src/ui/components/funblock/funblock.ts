3174e73551c4d3aefd144aab45c97207366e8987
deeppines <atheus92@gmail.com>
2025-01-24 18:44:20 +0300

    wip: fun block

---
+++ src/ui/components/funblock/funblock.ts
@@ -0,0 +1,9 @@
+import styles from './funblock.module.scss';
+
+const funblock = () => `
+  <div class="${styles.root}">
+    <span class="${styles.text}"></span>
+  </div>
+`;
+
+export default funblock;
