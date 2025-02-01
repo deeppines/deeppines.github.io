a3ba642484bd6a7ddc9cb2646fa456e487a0a820
deeppines <atheus92@gmail.com>
2025-02-01 09:42:42 +0300

    refactor: set title util

--- src/ui/utils/setTitle.ts
+++ src/ui/utils/setTitle.ts
@@ -1,3 +1,7 @@
-export const setTitle = (title: string) => {
-  document.title = title;
+/**
+ * Updates the page title with the given string.
+ * @param newTitle The new title to apply to the page.
+ */
+export const setTitle = (newTitle: string): void => {
+  document.title = newTitle;
 };
