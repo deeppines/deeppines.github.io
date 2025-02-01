7761b9ba1d8d7a81fa11858e0e7b67a0c306daff
deeppines <atheus92@gmail.com>
2025-02-01 09:43:08 +0300

    refactor: get lang util

--- src/ui/utils/getLang.ts
+++ src/ui/utils/getLang.ts
@@ -1,9 +1,21 @@
-export function getLang() {
-  const defaultLang = 'ru';
+/**
+ * Returns the language of the user's browser or the language that was set using
+ * the {@link setLang} function.
+ *
+ * @returns The language of the user's browser or the language that was set using
+ * the {@link setLang} function.
+ */
+export function getLang(): string {
+  const defaultLanguage = 'ru';
 
-  return (
-    localStorage.getItem('lang') ||
-    document.documentElement.getAttribute('data-lang') ||
-    defaultLang
-  );
+  // First, try to get the language from local storage.
+  const langFromLocalStorage = localStorage.getItem('lang');
+
+  // If the language is not in local storage, try to get it from the document's
+  // lang attribute.
+  const langFromDocument = document.documentElement.getAttribute('data-lang');
+
+  // If the language is not in local storage or the document's lang attribute,
+  // use the default language.
+  return langFromLocalStorage || langFromDocument || defaultLanguage;
 }
