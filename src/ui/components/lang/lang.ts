9327ad8a8b059d65885365c689467987064d2350
deeppines <atheus92@gmail.com>
2025-02-12 20:11:52 +0300

    refactor: lang select

--- src/ui/components/lang/lang.ts
+++ src/ui/components/lang/lang.ts
@@ -6,9 +6,16 @@ const lang = () => {
   const initLang = getLang();
 
   return `
-    <select class="${styles.root} js-lang-toggle" title="Language" value="${initLang}">
-      <option title="Russian" value="ru" ${initLang === 'ru' ? 'selected="selected"' : ''}>RU</option>
-      <option title="English" value="en" ${initLang === 'en' ? 'selected="selected"' : ''}>EN</option>
+    <select class="${styles.root} js-lang-toggle"
+      ${initLang === 'ru' ? 'title="Язык"' : 'title="Language"'}
+      value="${initLang}"
+    >
+      <option title="Russian" value="ru" ${initLang === 'ru' ? 'selected="selected"' : ''}>
+        RU
+      </option>
+      <option title="English" value="en" ${initLang === 'en' ? 'selected="selected"' : ''}>
+        EN
+      </option>
     </select>
   `;
 };
