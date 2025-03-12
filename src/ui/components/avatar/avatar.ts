2a998c4667de28cf72fbbbdcb48ff57a18066344
deeppines <atheus92@gmail.com>
2025-03-12 20:05:01 +0300

    refactor: avatar

--- src/ui/components/avatar/avatar.ts
+++ src/ui/components/avatar/avatar.ts
@@ -2,13 +2,17 @@ import styles from './avatar.module.scss';
 
 const avatar = (src?: string) => {
   return `
-    <div class="${styles.root}">
+    <div class="${styles.root} js-hidden-root">
       <img
         src="${src}"
         width="108"
         height="108"
         alt="Avatar"
       />
+
+      <button type="button" class="${styles.button} js-hidden-btn js-modal-me-open">
+        А тут что?
+      </button>
     </div>
   `;
 };
