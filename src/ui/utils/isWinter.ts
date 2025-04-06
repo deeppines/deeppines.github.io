1bd72f5fd1e0f326b9dff656e58f7eccaafccbda
deeppines <atheus92@gmail.com>
2025-04-06 20:40:56 +0300

    refactor: utils

--- src/ui/utils/isWinter.ts
+++ src/ui/utils/isWinter.ts
@@ -1,3 +1,7 @@
+/**
+ * Checks if the current month is between November and February
+ * @returns {boolean} true if winter, false otherwise
+ */
 export const isWinter = (): boolean => {
   const now = new Date();
   return now.getMonth() >= 11 || now.getMonth() < 2;
