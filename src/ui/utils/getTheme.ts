e7b716718357edf624b47795129f9ee26027c893
Egor K <kirdyashkin@aeroidea.ru>
2026-07-17 11:57:31 +0300

    refactor: theme

--- src/ui/utils/getTheme.ts
+++ src/ui/utils/getTheme.ts
@@ -1,9 +1,24 @@
+import type { Theme } from '@/types/common';
+
 import { DATA_ATTRIBUTES, STORAGE_KEYS } from '@/shared/domHooks';
 
-export const getTheme = (): string => {
-  return (
-    document.documentElement.getAttribute(DATA_ATTRIBUTES.theme) ||
-    localStorage.getItem(STORAGE_KEYS.theme) ||
-    'dark'
-  );
+const isTheme = (value: string | null): value is Theme => value === 'dark' || value === 'light';
+
+const getSystemTheme = (): Theme => {
+  return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+};
+
+export const getTheme = (): Theme => {
+  const themeFromDocument = document.documentElement.getAttribute(DATA_ATTRIBUTES.theme);
+  const themeFromStorage = localStorage.getItem(STORAGE_KEYS.theme);
+
+  if (isTheme(themeFromDocument)) {
+    return themeFromDocument;
+  }
+
+  if (isTheme(themeFromStorage)) {
+    return themeFromStorage;
+  }
+
+  return getSystemTheme();
 };
